08316a933dc7a7c1c9f7be858568f228e37ce381
AK <raj@ymirlabs.com>
2023-03-02 16:36:04 +0530

    Added samples for Vuex usage

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,47 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    count: 0,
+    text: "random"
   },
   getters: {
+    getSquaredValue (state) {
+      return state.count * state.count;
+    }
   },
   mutations: {
+    // We can not trigger asynchronous code inside mutation, only synchronous code is supported 
+    increaseCounter (state) {
+      state.count++;
+    },
+    decreaseCounter (state) {
+      state.count--;
+    },
+    setText (state, text) {
+      state.text = text;
+      console.log(state.text)
+    },
   },
   actions: {
+    // Here we can write asynchronous code! 
+    increaseCounter () {
+      console.log("Action dispatched!")
+    },
+    getToDos ({commit}) {
+      axios({
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/todos/1"
+      })
+        .then(res => {
+          console.log(res);
+          commit('setText', res.data.title);
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+  <div class="counter">
+    <div class="count">
+      {{ $store.state.count }}
+    </div>
+    <div class="buttons">
+      <!-- Mutation -->
+      <p>Here we are committing a mutation</p>
+      <button @click="$store.commit('increaseCounter')">
+        +
+      </button>
+      <button @click="$store.commit('decreaseCounter')">
+        -
+      </button>
+
+      <!-- Action dispatching -->
+      <p>Here we are dispatching an action</p>
+      <button @click="$store.dispatch('increaseCounter')">
+        +
+      </button>
+      <button @click="$store.commit('decreaseCounter')">
+        -
+      </button>
+    </div>
+  </div>
+  <!-- Asynchronous operation -->
+  <div class="api">
+    <button @click="$store.dispatch('getToDos')" class="api-button">
+      Fetch from Api
+    </button>
+  </div>
+
+  <!-- Using Vuex Getters -->
+  <div class="display-square">
+    <p>Here we are using getters</p>
+    {{ $store.state.count }} <sup>2</sup> = {{ $store.getters.getSquaredValue }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  // data() {
+  //   return {
+  //     count: 0,
+  //   }
+  // },
+  // methods: {
+  //   increaseCounter () {
+  //     this.count++;
+  //   },
+  //   decreaseCounter () {
+  //     this.count--;
+  //   }
+  // }
 }
 </script>
+
+<style scoped>
+  .count {
+    font-size: 100px;
+  }
+  button {
+    cursor: pointer;
+    font-size: 50px;
+    width: 80px;
+    margin: 0 10px;
+  }
+  .api-button {
+    width: 365px;
+    margin-top: 20px;
+    background: #fafafa;
+    border: 1px solid blue;
+    border-radius: 20px;
+  }
+</style>
